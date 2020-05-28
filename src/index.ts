551fa1364dadc70292dfee815d1596f4c1607dcd
Ben Gardiner <bennygards@gmail.com>
2020-05-28 11:08:34 +1000

    starting work on Ts today

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ class Bookmark {
     }
 
     setUrl(url:string){
+        // when you click the add button it should open up a new tile with an add button - when you add the url into the feild it souhld be into the argu
     }
 
     getUrl():string {
         return "";
+        // this shoould be get the link in the a tag on goto
     }
 
     
