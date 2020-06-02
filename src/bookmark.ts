83f00c6e48ac54371b7e29a76e42daee9ab3f9e0
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-06-02 15:48:30 +1000

    added ability to create text inputs to divs X:)

--- src/bookmark.ts
+++ src/bookmark.ts
@@ -6,16 +6,16 @@ export class Bookmark {
     constructor(title: string, favorite: boolean) {
         this.title = title;
         this.favorite = favorite;
-        
+
     }
 
-    setUrl(newUrl:string){
-         this.url = newUrl;
+    setUrl(newUrl: string) {
+        this.url = newUrl;
         ///use this to set tthe url initially
     }
 
-    getUrl():string {
-        
+    getUrl(): string {
+
         return this.url;
         // gets the url stored in the object box form the element on the page
     }
@@ -24,26 +24,34 @@ export class Bookmark {
 }
 
 export class CreateElementBookMark {
-        bookmark: Bookmark;
+    bookmark: Bookmark;
+
+
+    constructor(bookmark: Bookmark) {
+        this.bookmark = bookmark;
+
+    }
+
+    createBookMark(id: string) {
         
-        
-        constructor(bookmark:Bookmark) {
-            this.bookmark = bookmark;
+        var bkm1 = document.createElement("div");
+        bkm1.id = "urlBox";
+        var bkm2 = document.createElement("div");
+        bkm2.id = 'TitleBox';
+        document.getElementById(id)?.appendChild(bkm1);
+        document.getElementById(id)?.appendChild(bkm2);
+        document.getElementById('urlBox')?.appendChild(document.createTextNode(this.bookmark.title))
+        document.getElementById('urlBox')?.appendChild(document.createElement("br"))
+        document.getElementById('TitleBox')?.appendChild(document.createTextNode(this.bookmark.getUrl()))
 
-        }
-
-        createBookMark(id: string) {
-            let bkm1 = document.createElement("div") as HTMLDivElement| null;
-            
-            document.getElementById(id)?.appendChild(document.createTextNode(this.bookmark.title))
-            document.getElementById(id)?.appendChild(document.createTextNode(this.bookmark.getUrl()))
-            
-        }
-
-    } 
+    }
 
 
 
+}
 
-    
+
+
+
+
 
